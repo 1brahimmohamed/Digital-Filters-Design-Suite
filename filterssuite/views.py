e0b105cace049ce8bf957e3949f811c006f088ab
Mahmoud Yaser Salman <88372358+mahmoud1yaser@users.noreply.github.com>
2023-02-04 14:35:53 +0200

    feat: filter mag&phase

--- filterssuite/views.py
+++ filterssuite/views.py
@@ -19,13 +19,21 @@ def create(request):
     if request.method == 'POST':
         # file = request.FILES['file']
 
-        # temp zeros & poles
+        # temporary zeros & poles
         zeros = [{1, 2}, {0.2, 1}]
         poles = [{0.1, 2}, {2, 1}]
-        modified_zeros = []
-        modified_poles = []
+        complex_zeros = []
+        complex_poles = []
 
-        # digital_filter = DigitalFilter()
+        for z in zeros:
+            complex_zeros.append(complex(list(z)[0], list(z)[1]))
+        for p in poles:
+            complex_poles.append(complex(list(p)[0], list(p)[1]))
+
+        digital_filter = DigitalFilter(complex_zeros, complex_poles)
+        normalized_freq, magnitude, phase = digital_filter.response()
+
+        # plot in frontend normalized_freq (0.0 => pi) in x_axis, magnitude or phase in y_axis
 
         return JsonResponse(
             {
