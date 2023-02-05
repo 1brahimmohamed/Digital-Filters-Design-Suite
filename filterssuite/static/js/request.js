4cf62a139ddf6e893df423853f0260b6fab401bd
Ibrahim <imimazim7@gmail.com>
2023-02-05 02:17:53 +0200

    docs: add gitattributes

--- filterssuite/static/js/request.js
+++ filterssuite/static/js/request.js
@@ -8,36 +8,30 @@
  *******************************************************************************/
 
 const setUpRequestData = __ => {
-    let zeros = [],
-        poles = [];
+    let zeros = [], poles = [];
 
     for (let i = 0; i < unitCircleBoard.getZeroes.length; i++) {
-        let zero = unitCircleBoard.getZeroes[i],
-            zeroX = zero.x(),
-            zeroY = zero.y()
+        let zero = unitCircleBoard.getZeroes[i], zeroX = zero.x(), zeroY = zero.y()
 
-        let normalizedX = + ((zeroX - unitCircleBoard.getCircleCenterX) / unitCircleBoard.getCircleRadius).toFixed(4),
-            normalizedY = - ((zeroY - unitCircleBoard.getCircleCenterY) / unitCircleBoard.getCircleRadius).toFixed(4);
+        let normalizedX = +((zeroX - unitCircleBoard.getCircleCenterX) / unitCircleBoard.getCircleRadius).toFixed(4),
+            normalizedY = -((zeroY - unitCircleBoard.getCircleCenterY) / unitCircleBoard.getCircleRadius).toFixed(4);
 
         let zeroFilterData = {
-            x: normalizedX,
-            y: normalizedY,
+            x: normalizedX, y: normalizedY,
         }
 
         zeros.push(zeroFilterData);
     }
 
     for (let i = 0; i < unitCircleBoard.getPoles.length; i++) {
-        let pole = unitCircleBoard.getPoles[i],
-            poleX = pole.x() + pole.width() / 2,
+        let pole = unitCircleBoard.getPoles[i], poleX = pole.x() + pole.width() / 2,
             poleY = pole.y() + pole.height() / 2;
 
-        let normalizedX = + ((poleX - unitCircleBoard.getCircleCenterX) / unitCircleBoard.getCircleRadius).toFixed(4),
-            normalizedY = - ((poleY - unitCircleBoard.getCircleCenterY) / unitCircleBoard.getCircleRadius).toFixed(4);
+        let normalizedX = +((poleX - unitCircleBoard.getCircleCenterX) / unitCircleBoard.getCircleRadius).toFixed(4),
+            normalizedY = -((poleY - unitCircleBoard.getCircleCenterY) / unitCircleBoard.getCircleRadius).toFixed(4);
 
         let poleFilterData = {
-            x: normalizedX,
-            y: normalizedY,
+            x: normalizedX, y: normalizedY,
         }
 
         poles.push(poleFilterData);
@@ -52,20 +46,23 @@ const sendRequest = __ => {
     setUpRequestData();
 
     $.ajax({
-        url: 'http://127.0.0.1:8000/suite/get-filter-response/',
-        type: 'POST',
-        headers: {
+        url: 'http://127.0.0.1:8000/suite/get-filter-response/', type: 'POST', headers: {
             "Content-Type": "application/json"
-        },
-        dataType: 'json',
-        async: false,
-        data: JSON.stringify({
-            zeros: currentFilter.getZeros,
-            poles: currentFilter.getPoles,
-        }),
-        success: function (response) {
-            magnitudePlot.updatePlot({x: response.normalizedFrequency, y: response.magnitudeResponse});
-            phasePlot.updatePlot({x: response.normalizedFrequency, y: response.phaseResponse});
+        }, dataType: 'json', async: false, data: JSON.stringify({
+            zeros: currentFilter.getZeros, poles: currentFilter.getPoles,
+        }), success: function (response) {
+
+            magnitudePlot.updatePlot(
+                {
+                        x: response.normalizedFrequency,
+                        y: response.magnitudeResponse
+                });
+
+            phasePlot.updatePlot(
+                {
+                        x: response.normalizedFrequency,
+                        y: response.phaseResponse
+                });
         }
     })
 
